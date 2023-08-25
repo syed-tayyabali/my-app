8fa02fa87d0881c0fe75bcf1ce3265fd2f68b5f7
syed-tayyabali <tayyab.ali98@hotmail.com>
2023-08-25 23:37:53 +0500

    added MUI vertical tabs to exprience file.

--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -2,3 +2,10 @@ export const colors = {
   theme: "#212121",
   orange: "#bf360c",
 };
+
+export const a11yProps = (index: number) => {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
+};
--- src/Containers/Exprience/index.tsx
+++ src/Containers/Exprience/index.tsx
@@ -1,8 +1,20 @@
 import { Divider, Grid, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import * as React from "react";
+import { TabPanel } from "../../Components/TabPanel";
+import { a11yProps } from "../../Constants";
 
 export const Exprience = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
-    <section style={{ width: "100%" }}>
+    <section style={{ width: "100%", marginBottom: "50px" }}>
       <Grid container spacing={3} pb={8}>
         <Grid item xs={3}>
           <Divider
@@ -27,6 +39,58 @@ export const Exprience = () => {
           />
         </Grid>
       </Grid>
+
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+        }}
+      >
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          sx={{ borderRight: 1, borderColor: "divider" }}
+          TabIndicatorProps={{
+            style: {
+              backgroundColor: "#bf360c",
+            },
+          }}
+        >
+          <Tab
+            style={{
+              color: value === 0 ? "#bf360c" : "",
+            }}
+            label="Item One"
+            {...a11yProps(0)}
+          />
+          <Tab
+            style={{
+              color: value === 1 ? "#bf360c" : "",
+            }}
+            label="Item Two"
+            {...a11yProps(1)}
+          />
+          <Tab
+            style={{
+              color: value === 2 ? "#bf360c" : "",
+            }}
+            label="Item Three"
+            {...a11yProps(2)}
+          />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+      </Box>
     </section>
   );
 };
